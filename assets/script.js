c2c1d0591434c74040df1aa2b6df037b5b16632e
samt11345 <Samuel.tahjai.williams@gmail.com>
2023-07-16 18:22:34 -0400

    tinkering

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,24 @@
-// var apiKey = "5fe1dca8";
-// var apiUrl = "http://www.omdbapi.com/?apikey=" + apiKey;
+var apiKey = "5fe1dca8";
+var apiUrl = "http://www.omdbapi.com/?apikey=" + apiKey;
+var movieDetailsElement = document.getElementById("movieDetails");
 
-// function searchMovie(title) {
-//     fetch(`${apiUrl}&t=${encodeURIComponent(title)}`)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//         })
-//         .catch(function (error) {
-//             console.log("Error:", error);
-//         });
-// }
+function searchMovie() {
+    var title = document.getElementById("movieTitle").value;
 
-// // Example usage:
-// searchMovie("Inception");
-
-    var apiKey = "5fe1dca8";
-    var apiUrl = "http://www.omdbapi.com/?apikey=" + apiKey;
-    var movieDetailsElement = document.getElementById("movieDetails");
-
-    function searchMovie() {
-      var title = document.getElementById("movieTitle").value;
-
-      fetch(`${apiUrl}&t=${encodeURIComponent(title)}`)
+    fetch(`${apiUrl}&t=${encodeURIComponent(title)}`)
         .then(function (response) {
-          return response.json();
+            return response.json();
         })
         .then(function (data) {
-          displayMovieDetails(data);
+            displayMovieDetails(data);
         })
         .catch(function (error) {
-          console.log("Error:", error);
+            console.log("Error:", error);
         });
-    }
+}
 
-    function displayMovieDetails(movie) {
-      if (movie.Response === "True") {
+function displayMovieDetails(movie) {
+    if (movie.Response === "True") {
         var html = `
           <h2>${movie.Title}</h2>
           <p>Year: ${movie.Year}</p>
@@ -45,8 +26,45 @@
           <p>Plot: ${movie.Plot}</p>
         `;
         movieDetailsElement.innerHTML = html;
-      } else {
+    } else {
         movieDetailsElement.innerHTML = `<p>${movie.Error}</p>`;
-      }
     }
+}
 
+// watch mode api information down below
+// var wmApiKey = TNJADaKq3XlXMkATsndQthQewGSmwIfLUdLVrqez;
+// var wmApiUrl = "https://api.watchmode.com/datasets/title_id_map.csv" + wmApiKey;
+
+
+
+
+
+function searchMovie() {
+    var movieTitle = document.getElementById("movieTitle").value;
+
+    // Make a request to the OMDB API
+    var omdbApiKey = "5fe1dca8";
+    var omdbApiUrl = `http://www.omdbapi.com/?apikey=${omdbApiKey}&t=${encodeURIComponent(movieTitle)}`;
+
+    fetch(omdbApiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        displayMovieDetails(data);
+      })
+      .catch(function (error) {
+        console.log("Error:", error);
+      });
+  }
+
+  function displayMovieDetails(movie) {
+    var movieDetailsElement = document.getElementById("movieDetails");
+    movieDetailsElement.innerHTML = `
+      <h2>${movie.Title}</h2>
+      <img src="${movie.Poster}" alt="${movie.Title} Poster">
+      <p>Year: ${movie.Year}</p>
+      <p>Director: ${movie.Director}</p>
+      <p>Plot: ${movie.Plot}</p>
+    `;
+  }
